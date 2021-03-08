65abb04343f342752aa4dc18610067643d653176
aiko-bazarkulova <80278491+aiko-bazarkulova@users.noreply.github.com>
2021-03-09 04:41:37 +0600

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Student extends Human {
+    String facultyNumber;
+    public String getFacultyNumber() {
+        return facultyNumber;
+    }
+    public void setFacultyNumber(String facultyNumber) {
+        this.facultyNumber = facultyNumber;
+    }
+}
